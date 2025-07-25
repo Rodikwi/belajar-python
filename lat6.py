8dffac962e8ccfa69848455d86ac095a298727f6
Rodik Indrawan <rodik.w.i@ftmm.unair.ac.id>
2025-07-25 08:58:00 +0700

    menambahkan lat6

---
+++ lat6.py
@@ -0,0 +1,24 @@
+def nama_fungsi(parameter1, parameter2):
+    # blok kode
+    return hasil
+
+def sapa(nama):
+    print("Halo,", nama)
+
+sapa("Rodik")
+
+def jumlah(a, b):
+    return a + b
+
+hasil = jumlah(3, 5)
+print("Hasil:", hasil)
+
+def cek_genap(angka):
+    if angka % 2 == 0:
+        return "Genap"
+    else:
+        return "Ganjil"
+
+a = int(input("Masukkan angka: "))
+print("Angka kamu adalah:", cek_genap(a))
+
