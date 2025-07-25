23e86bdfda762f3980ab3d8aeb46f103af7718fb
Rodik Indrawan <rodik.w.i@ftmm.unair.ac.id>
2025-07-25 08:22:25 +0700

    menambah lat4

---
+++ lat3.py
@@ -0,0 +1,8 @@
+angka = int(input("Masukkan sebuah angka: "))
+
+if angka > 0:
+    print("Angka tersebut adalah positif.")
+elif angka < 0:
+    print("Angka tersebut adalah negatif.")
+else:
+    print("Angka tersebut adalah nol.")
---
+++ lat4.py
@@ -0,0 +1,10 @@
+nilai = int(input("Masukkan nilai ujian kamu (0-100) : "))
+
+if nilai >=80 :
+    print("lulus dengan A")
+elif nilai >= 70:
+    print("lulus dengan B")
+elif nilai >= 60:
+    print("lulus dengan C")
+else :
+    print("Tidak Lulus")
