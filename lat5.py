dccc3b4c5c9f49cbbbad02d6201f4ef43d09e8d7
Rodik Indrawan <rodik.w.i@ftmm.unair.ac.id>
2025-07-25 08:28:32 +0700

    menambahkan lat.5.py

---
+++ lat5.py
@@ -0,0 +1,15 @@
+for i in range (1,6):
+    print("anhgka :",i)
+
+nama = "Rodik"
+for huruf in nama:
+    print(huruf)
+
+i = 1
+while i <= 5:
+    print("Angka:", i)
+    i += 1
+
+jawaban = ""
+while jawaban != "iya":
+    jawaban = input("Apakah kamu mau lanjut? (iya/tidak): ")
