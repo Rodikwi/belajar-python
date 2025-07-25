d93976d36c94fe01f06e98835dc484601811106f
Rodik Wahyu Indrawan <rodik.w.i@ftmm.unair.ac.id>
2025-07-25 14:44:47 +0700

    update lat7

--- lat7.py
+++ lat7.py
@@ -6,3 +6,13 @@ def cek_genap(angka):
 
 a = int(input("Masukkan angka: "))
 print("Angka kamu adalah:", cek_genap(a))
+
+
+def cek_genap(angka):
+    if angka % 2 == 0:
+        return "Genap"
+    else:
+        return "Ganjil"
+
+a = int(input("Masukkan angka: "))
+print("Angka kamu adalah:", cek_genap(a))
